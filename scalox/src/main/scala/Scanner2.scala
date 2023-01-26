11de43e182f20fbf89ec5c96d310d88515e59bd2
ayhon <43295942+ayhon@users.noreply.github.com>
2023-01-27 00:51:47 +0100

    Implement tokens with an Enum

---
+++ scalox/src/main/scala/Scanner2.scala
@@ -0,0 +1,117 @@
+package lox
+
+import Token2._
+
+val keywords = Map[String,Int => Token2](
+  "and"    -> And.apply,
+  "class"  -> Class.apply,
+  "else"   -> Else.apply,
+  "false"  -> False.apply,
+  "for"    -> For.apply,
+  "fun"    -> Fun.apply,
+  "if"     -> If.apply,
+  "nil"    -> Nil.apply,
+  "or"     -> Or.apply,
+  "print"  -> Print.apply,
+  "return" -> Return.apply,
+  "super"  -> Super.apply,
+  "this"   -> This.apply,
+  "true"   -> True.apply,
+  "var"    -> Var.apply,
+  "while"  -> While.apply,
+)
+
+case class Scanner2(val source: String):
+  var tokens = Array[Token2]()
+
+  private var start: Int = 0;
+  private var current: Int = 0;
+  private var line: Int = 1;
+
+  def scanTokens(): Array[Token2] =
+    while !isAtEnd do
+      start = current
+      scanToken()
+    tokens :+= EndOfFile(line)
+    tokens
+
+  def isAtEnd: Boolean = current >= source.length
+
+  def scanToken() = advance() match
+    case '(' => LeftParen(line)
+    case ')' => RightParen(line)
+    case '{' => LeftBrace(line)
+    case '}' => RightBrace(line)
+    case ',' => Comma(line)
+    case '.' => Dot(line)
+    case '-' => Minus(line)
+    case '+' => Plus(line)
+    case ';' => Semicolon(line)
+    case '*' => Star(line)
+    case '!' => if matches('=')
+                then BangEqual(line) else Bang(line)
+    case '=' => if matches('=')
+                then EqualEqual(line) else Equal(line)
+    case '<' => if matches('=')
+                then LessEqual(line) else Less(line)
+    case '>' => if matches('=')
+                then GreaterEqual(line) else Greater(line)
+    case '/' => 
+      if matches('/') then 
+        while peek() != '\n' && !isAtEnd do advance()
+      else
+        Slash(line)
+    case ' '|'\r'|'\t' => () // Ignore whitespace
+    case '\n' => line += 1;
+    case '\"' => string()
+    case num if isDigit(num) => number()
+    case letter if isAlpha(letter) => identifier()
+    case unexpected =>
+      Lox.error(line, s"Unexpected character $unexpected")
+
+  def matches(expected: Char) =
+    val res = !isAtEnd && peek() == expected
+    if res then
+      current += 1
+    res
+    
+  def peek() = if isAtEnd then 0.toChar else source.charAt(current)
+  def peekNext() = if (current +1 >= source.length) then 0.toChar else source.charAt(current+1)
+  def advance() = 
+    val character = peek()
+    current+=1
+    character
+
+  def getText() = source.substring(start, current)
+
+  def string() =
+    while !isAtEnd && peek() != '\"' do
+      if peek() == '\n' then
+        line += 1
+      advance()
+
+    if isAtEnd then
+      Lox.error(line, "Unterminated string")
+    else
+      advance()
+      // Trim surrounding quotes
+      val value = source.substring(start+1,current-1)
+      String_(getText(), value, line)
+
+  def isDigit(c: Char): Boolean = c.isDigit
+  def number() =
+    while isDigit(peek()) do advance()
+    if peek() == '.' && isDigit(peekNext()) then
+      advance()
+      while isDigit(peek()) do advance()
+    Number_(getText(), getText().toDouble, line)
+    
+
+  def isAlpha(c: Char): Boolean = c.isUpper || c.isLower || c == '_'
+  def isAlphaNumeric(c: Char): Boolean = isAlpha(c) || isDigit(c)
+  def identifier() =
+    while isAlphaNumeric(peek()) do advance()
+    val text = source.substring(start, current)
+    keywords.get(text) match 
+      case Some(token) => token(line)
+      case None => Identifier(getText(), line)
---
+++ scalox/src/main/scala/Token2.scala
@@ -0,0 +1,54 @@
+package lox
+
+enum Token2(val lexeme: String, val literal: Any, val line: Int):
+  // Single character tokens
+  case LeftParen(ln: Int)                       extends Token2("(", null, ln)
+  case RightParen(ln: Int)                      extends Token2(")", null, ln)
+  case LeftBrace(ln: Int)                      extends Token2("{", null, ln)
+  case RightBrace(ln: Int)                     extends Token2("}", null, ln)
+  case Comma(ln: Int)                           extends Token2(",", null ,ln)
+  case Dot(ln: Int)                             extends Token2(".", null ,ln)
+  case Minus(ln: Int)                           extends Token2("-", null ,ln)
+  case Plus(ln: Int)                            extends Token2("+", null ,ln)
+  case Semicolon(ln: Int)                       extends Token2(":", null ,ln)
+  case Slash(ln: Int)                           extends Token2("/", null ,ln)
+  case Star(ln: Int)                            extends Token2("*", null ,ln)
+ 
+  // One or two character tokens
+  case Bang(ln: Int)                            extends Token2("!",  null ,ln)
+  case BangEqual(ln: Int)                       extends Token2("!=", null ,ln)
+  case Equal(ln: Int)                           extends Token2("=",  null ,ln)
+  case EqualEqual(ln: Int)                      extends Token2("==", null ,ln)
+  case Greater(ln: Int)                         extends Token2(">",  null ,ln)
+  case GreaterEqual(ln: Int)                    extends Token2(">=", null ,ln)
+  case Less(ln: Int)                            extends Token2("<",  null ,ln)
+  case LessEqual(ln: Int)                       extends Token2("<=", null ,ln)
+
+  // ltrls
+  case Identifier(lxm: String, ln: Int)         extends Token2(lxm, null ,ln)
+  case String_(lxm: String, ltrl: Any, ln: Int) extends Token2(lxm, ltrl ,ln)
+  case Number_(lxm: String, ltrl: Any, ln: Int) extends Token2(lxm, ltrl ,ln)
+
+  // Keywords
+  case And(ln: Int)                             extends Token2("and", null, ln)
+  case Class(ln: Int)                           extends Token2("class", null, ln)
+  case Else(ln: Int)                            extends Token2("else", null, ln)
+  case False(ln: Int)                           extends Token2("false", null, ln)
+  case Fun(ln: Int)                             extends Token2("fun", null, ln)
+  case For(ln: Int)                             extends Token2("for", null, ln)
+  case If(ln: Int)                              extends Token2("if", null, ln)
+  case Nil(ln: Int)                             extends Token2("nil", null, ln)
+  case Or(ln: Int)                              extends Token2("or", null, ln)
+  case Print(ln: Int)                           extends Token2("print", null, ln)
+  case Return(ln: Int)                          extends Token2("return", null, ln)
+  case Super(ln: Int)                           extends Token2("super", null, ln)
+  case This(ln: Int)                            extends Token2("this", null, ln)
+  case True(ln: Int)                            extends Token2("true", null, ln)
+  case Var(ln: Int)                             extends Token2("var", null, ln)
+  case While(ln: Int)                           extends Token2("while", null, ln)
+  
+
+  case EndOfFile(ln: Int)                       extends Token2("", null, ln)
+
+
+
