29cc9c31bf726891c14f41541c575d87c83d8355
ayhon <43295942+ayhon@users.noreply.github.com>
2023-01-28 00:00:25 +0100

    Add test for scanner

--- scalox/src/test/scala/MySuite.scala
+++ scalox/src/test/scala/MySuite.scala
@@ -1,9 +1,87 @@
+import lox._
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+  test("scanner gives tokens for small code snippet") {
+    val source = """
+var x = 100;
+var y = 200;
+for(var i = 0; i < 10; i = i + 1) {
+  func add(a,b) { return a + b; }
+  print add((x+i)/(y-i));
+  print "And done!";
+}"""
+    val expected = Array(
+      "var\tVar",
+      "x\tIdentifier",
+      "=\tEqual",
+      "100\tNumber_\t100.0",
+      ";\tSemicolon",
+      "var\tVar",
+      "y\tIdentifier",
+      "=\tEqual",
+      "200\tNumber_\t200.0",
+      ";\tSemicolon",
+      "for\tFor",
+      "(\tLeftParen",
+      "var\tVar",
+      "i\tIdentifier",
+      "=\tEqual",
+      "0\tNumber_\t0.0",
+      ";\tSemicolon",
+      "i\tIdentifier",
+      "<\tLess",
+      "10\tNumber_\t10.0",
+      ";\tSemicolon",
+      "i\tIdentifier",
+      "=\tEqual",
+      "i\tIdentifier",
+      "+\tPlus",
+      "1\tNumber_\t1.0",
+      ")\tRightParen",
+      "{\tLeftBrace",
+      "func\tIdentifier",
+      "add\tIdentifier",
+      "(\tLeftParen",
+      "a\tIdentifier",
+      ",\tComma",
+      "b\tIdentifier",
+      ")\tRightParen",
+      "{\tLeftBrace",
+      "return\tReturn",
+      "a\tIdentifier",
+      "+\tPlus",
+      "b\tIdentifier",
+      ";\tSemicolon",
+      "}\tRightBrace",
+      "print\tPrint",
+      "add\tIdentifier",
+      "(\tLeftParen",
+      "(\tLeftParen",
+      "x\tIdentifier",
+      "+\tPlus",
+      "i\tIdentifier",
+      ")\tRightParen",
+      "/\tSlash",
+      "(\tLeftParen",
+      "y\tIdentifier",
+      "-\tMinus",
+      "i\tIdentifier",
+      ")\tRightParen",
+      ")\tRightParen",
+      ";\tSemicolon",
+      "print\tPrint",
+      "\"And done!\"\tString_\tAnd done!",
+      ";\tSemicolon",
+      "}\tRightBrace",
+      "\tEndOfFile"
+    )
+    val scanner: Scanner2 = new Scanner2(source)
+    val tokens: Array[Token2] = scanner.scanTokens()
+
+    // println(source)
+    var s = scala.collection.mutable.StringBuilder()
+    for (token, expect) <- tokens.zip(expected) do
+      assertNoDiff(token.toString, expect.toString)
   }
 }
